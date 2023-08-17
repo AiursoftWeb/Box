10a4e1a997ecb6fdfc931068ac4c57067bd8e771
Anduin Xue <anduin@aiursoft.com>
2023-08-17 13:00:21 +0000

    New staging.

--- start.sh
+++ start.sh
@@ -8,12 +8,15 @@ rm -rf ./stage/configuration
 sleep 1
 
 echo "Staging configuration..."
+mkdir -p ./stage
+mkdir -p ./stage/configuration
+mkdir -p ./stage/datastore
+mkdir -p ./stage/logs
 cp -r ./declare/configuration ./stage/
 sleep 1
 
 echo "Setting permissions..."
-sudo chmod -R 777 ./stage/datastore
-sudo chmod -R 777 ./stage/logs
+sudo chmod -R 777 ./stage
 sleep 3
 
 echo "Starting..."
