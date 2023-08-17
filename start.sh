a7faf626ac7e33ef3fb411670f7ed58aaba409cf
Anduin Xue <anduin@aiursoft.com>
2023-08-17 08:56:05 +0000

    Init.

---
+++ start.sh
@@ -0,0 +1,15 @@
+echo "Stopping docker cluster..."
+docker-compose stop
+docker-compose rm -f
+sleep 1
+
+echo "Cleaning staged configuration..."
+rm -rf ./stage/configuration
+sleep 1
+
+echo "Staging configuration..."
+cp -r ./declare/configuration ./stage/
+sleep 1
+
+echo "Starting..."
+docker-compose up
