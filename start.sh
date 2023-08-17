6a7431922c81e68510a30898c09f7deb83bcf139
Anduin Xue <anduin@aiursoft.com>
2023-08-17 09:25:10 +0000

    Add grafana.

--- start.sh
+++ start.sh
@@ -9,7 +9,11 @@ sleep 1
 
 echo "Staging configuration..."
 cp -r ./declare/configuration ./stage/
-sleep 3
+sleep 1
+
+echo "Setting permissions..."
+sudo chmod -R 777 ./stage/datastore
+sudo chmod -R 777 ./stage/logs
 
 echo "Starting..."
 docker-compose up
