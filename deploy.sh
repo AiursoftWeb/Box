1ce521d3fbd660f5395d9072fb1e30de068a8d1c
AnduinXue <anduin@aiursoft.com>
2024-03-10 16:32:50 +0000

    Fix name.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 sudo docker stack deploy -c swarmpit/docker-compose.yml swarmpit # Ports 888
 sudo docker stack deploy -c tracer/docker-compose.yml tracer # Ports 48466
-sudo docker stack deploy -c manhours/docker-compose.yml tracer # Ports 48467
+sudo docker stack deploy -c manhours/docker-compose.yml manhours # Ports 48467
