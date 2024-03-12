f4f4f47995a18b3059ce8348e1ae41df235c9cfc
AnduinXue <anduin@aiursoft.com>
2024-03-12 14:01:28 +0000

    add health

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@ sudo docker stack deploy -c tracer/docker-compose.yml tracer # Ports 48466
 sudo docker stack deploy -c manhours/docker-compose.yml manhours # Ports 48467
 sudo docker stack deploy -c chess/docker-compose.yml chess # Ports 48468
 sudo docker stack deploy -c stathub/docker-compose.yml stathub # Ports 48469
+sudo docker stack deploy -c health/docker-compose.yml health # Ports 48470 48471
