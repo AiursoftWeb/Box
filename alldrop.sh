b89ef39bc617ca8883b268b66e62b82e71473fce
AnduinXue <anduin@aiursoft.com>
2024-03-12 16:42:34 +0000

    simplify alldrop script

--- alldrop.sh
+++ alldrop.sh
@@ -1,20 +1,11 @@
-# sudo docker stack deploy -c swarmpit/docker-compose.yml swarmpit # Ports 888
-# sudo docker stack deploy -c tracer/docker-compose.yml tracer # Ports 48466
-# sudo docker stack deploy -c manhours/docker-compose.yml manhours # Ports 48467
-sudo docker stack rm swarmpit
-sudo docker stack rm tracer
-sudo docker stack rm manhours
+sudo docker stack rm $(sudo docker stack ls -q)
+sleep 10
 sudo docker kill $(sudo docker ps -q)
+sleep 10
 sudo docker rm $(sudo docker ps -q)
-sudo docker volume prune -f
+sleep 10
 sudo docker volume rm $(sudo docker volume ls -q)
-
-ssh anduin@sworker1 'sudo docker kill $(sudo docker ps -q)'
-ssh anduin@sworker1 'sudo docker rm $(sudo docker ps -q)'
-ssh anduin@sworker1 'sudo docker volume prune -f'
-ssh anduin@sworker1 'sudo docker volume rm $(sudo docker volume ls -q)'
-
-ssh anduin@sworker2 'sudo docker kill $(sudo docker ps -q)'
-ssh anduin@sworker2 'sudo docker rm $(sudo docker ps -q)'
-ssh anduin@sworker2 'sudo docker volume prune -f'
-ssh anduin@sworker2 'sudo docker volume rm $(sudo docker volume ls -q)'
+sleep 10
+sudo docker image rm $(sudo docker image ls -q)
+sleep 10
+sudo docker volume prune -f
