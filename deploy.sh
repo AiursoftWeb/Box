eed672a65e790bd3579c97a8e3a217b6e1163865
xuef <xuef@microsoft.com>
2024-03-12 10:20:25 +0000

    Fix typo.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ sudo docker stack deploy -c swarmpit/docker-compose.yml swarmpit # Ports 888
 sudo docker stack deploy -c tracer/docker-compose.yml tracer # Ports 48466
 sudo docker stack deploy -c manhours/docker-compose.yml manhours # Ports 48467
 sudo docker stack deploy -c chess/docker-compose.yml chess # Ports 48468
-sudo docker stack deploy -c stathub/docker-compose.yml chess # Ports 48469
+sudo docker stack deploy -c stathub/docker-compose.yml stathub # Ports 48469
