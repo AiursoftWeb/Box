57cecc7bb64be60553ee882609b52e6f10f2eb54
AnduinXue <anduin@aiursoft.com>
2024-03-12 15:21:57 +0000

    Add chat.

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ sudo docker stack deploy -c manhours/docker-compose.yml manhours # Ports 48467
 sudo docker stack deploy -c chess/docker-compose.yml chess # Ports 48468
 sudo docker stack deploy -c stathub/docker-compose.yml stathub # Ports 48469
 sudo docker stack deploy -c health/docker-compose.yml health # Ports 48470 48471
+sudo docker stack deploy -c chat/docker-compose.yml chat # Ports 48472
