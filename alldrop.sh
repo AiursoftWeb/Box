a960472e388edc56957a36532afa6e78808d5dec
xuef <xuef@microsoft.com>
2024-03-12 09:23:25 +0000

    Clean alldrop.

--- alldrop.sh
+++ alldrop.sh
@@ -5,22 +5,16 @@ sudo docker stack rm swarmpit
 sudo docker stack rm tracer
 sudo docker stack rm manhours
 sudo docker kill $(sudo docker ps -q)
+sudo docker rm $(sudo docker ps -q)
 sudo docker volume prune -f
 sudo docker volume rm $(sudo docker volume ls -q)
-sudo systemctl stop docker-store
-sudo rm /var/lib/docker-volumes/netshare/nfs -rf
-sudo systemctl start docker-store
 
 ssh anduin@sworker1 'sudo docker kill $(sudo docker ps -q)'
+ssh anduin@sworker1 'sudo docker rm $(sudo docker ps -q)'
 ssh anduin@sworker1 'sudo docker volume prune -f'
 ssh anduin@sworker1 'sudo docker volume rm $(sudo docker volume ls -q)'
-ssh anduin@sworker1 'sudo systemctl stop docker-store'
-ssh anduin@sworker1 'sudo rm /var/lib/docker-volumes/netshare/nfs -rf'
-ssh anduin@sworker1 'sudo systemctl start docker-store'
 
 ssh anduin@sworker2 'sudo docker kill $(sudo docker ps -q)'
+ssh anduin@sworker2 'sudo docker rm $(sudo docker ps -q)'
 ssh anduin@sworker2 'sudo docker volume prune -f'
 ssh anduin@sworker2 'sudo docker volume rm $(sudo docker volume ls -q)'
-ssh anduin@sworker2 'sudo systemctl stop docker-store'
-ssh anduin@sworker2 'sudo rm /var/lib/docker-volumes/netshare/nfs -rf'
-ssh anduin@sworker2 'sudo systemctl start docker-store'
