b91a3aaddb2e654b17ab6529a4226d576e4a4554
AnduinXue <anduin@aiursoft.com>
2024-03-16 10:41:06 +0000

    Swarmpit use persistent data.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
+# Swarmpit:
+sudo mkdir -p /mnt/docker/swarmpit/db-data
+sudo mkdir -p /mnt/docker/swarmpit/influx-data
 sudo docker stack deploy -c swarmpit/docker-compose.yml   swarmpit   # Ports 888
-sudo docker stack deploy -c tracer/docker-compose.yml     tracer     # Ports 48466
 
-sudo mkdir /mnt/docker/manhours # Shared NFS
+# Manhours:
+sudo mkdir -p /mnt/docker/manhours
 sudo docker stack deploy -c manhours/docker-compose.yml   manhours   # Ports 48467
+
+# Stateless:
+sudo docker stack deploy -c tracer/docker-compose.yml     tracer     # Ports 48466
 sudo docker stack deploy -c chess/docker-compose.yml      chess      # Ports 48468
 sudo docker stack deploy -c stathub/docker-compose.yml    stathub    # Ports 48469
 sudo docker stack deploy -c health/docker-compose.yml     health     # Ports 48470 48471
