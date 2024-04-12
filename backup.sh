79570ff5e4458f28aec74b41ce903ffef5947559
AnduinXue <anduin@aiursoft.com>
2024-04-12 15:43:48 +0000

    Add bandwidth limit to rsync command in backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,9 @@
 if [ -e /dev/sda2 ]; then
     sudo mkdir -p /mnt/backup
     sudo mount /dev/sda2 /mnt/backup
-    sudo rsync -Aavx --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
+    
+    # Limit bandwidth to 5MB/s to avoid IO congestion
+    sudo rsync -Aavx --bwlimit=5000 --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
     sudo umount /mnt/backup
 else
     echo "No /dev/sda found, skipping backup..."
