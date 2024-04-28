0a79706abe699cde68ef4b10c7116cadfeba4667
xuef <xuef@microsoft.com>
2024-04-28 14:56:52 +0000

    Refactor backup.sh script to remove bandwidth limit

--- backup.sh
+++ backup.sh
@@ -7,8 +7,7 @@ if [ -e /dev/sda ]; then
     sudo mkdir -p /mnt/backup
     sudo mount /dev/sda /mnt/backup
     
-    # Limit bandwidth to 5MB/s to avoid IO congestion
-    sudo rsync -Aavx --bwlimit=5000 --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
+    sudo rsync -Aavx --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
     sudo umount /mnt/backup
 else
     echo "No /dev/sda found, skipping backup..."
