8e233710547730f16252fbf9cefba3fe2d33d012
xuef <xuef@microsoft.com>
2024-04-28 14:55:50 +0000

    Update backup.sh script to decouple from /dev/sda

--- backup.sh
+++ backup.sh
@@ -1,6 +1,11 @@
-if [ -e /dev/sda2 ]; then
+#/bin/bash
+
+# TODO: Decouple this script with /dev/sda
+# This is because /dev/sda is not always the backup drive
+
+if [ -e /dev/sda ]; then
     sudo mkdir -p /mnt/backup
-    sudo mount /dev/sda2 /mnt/backup
+    sudo mount /dev/sda /mnt/backup
     
     # Limit bandwidth to 5MB/s to avoid IO congestion
     sudo rsync -Aavx --bwlimit=5000 --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
