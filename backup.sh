2a962f243f8ff7b17444e64c64fe7ba5a801b2e5
Anduin Xue <anduin@aiursoft.com>
2024-07-07 14:21:19 +0000

    chore: Update backup.sh to decouple from /dev/sdc
    This commit updates the backup.sh script to decouple it from the specific device /dev/sdc. Previously, the script was hardcoded to use /dev/sda as the backup drive, which is not always the case. Now, the script checks for the existence of /dev/sdc and uses it as the backup drive if available.

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
-# TODO: Decouple this script with /dev/sda
-# This is because /dev/sda is not always the backup drive
+# TODO: Decouple this script with /dev/sdc
+# This is because /dev/sdc is not always the backup drive
 
-if [ -e /dev/sda ]; then
+if [ -e /dev/sdc ]; then
     sudo mkdir -p /mnt/backup
-    sudo mount /dev/sda /mnt/backup
+    sudo mount /dev/sdc /mnt/backup
     
     sudo rsync -Aavx --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
     sudo umount /mnt/backup
 else
-    echo "No /dev/sda found, skipping backup..."
+    echo "No /dev/sdc found, skipping backup..."
     exit 1
 fi
