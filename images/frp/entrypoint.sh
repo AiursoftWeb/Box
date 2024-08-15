edac939fe002e65d9020db25529a73f14e09877b
Anduin Xue <anduin@aiursoft.com>
2024-08-15 15:57:27 +0000

    Update frpc.toml with new server address

--- images/frp/entrypoint.sh
+++ images/frp/entrypoint.sh
@@ -1,4 +1,5 @@
 token=$(cat /run/secrets/frp-token)
+serverip=$(echo $FRP_SERVER)
 
 # If token is empty, exit
 if [ -z "$token" ]; then
@@ -6,11 +7,19 @@ if [ -z "$token" ]; then
     exit 1
 fi
 
+if [ -z "$serverip" ]; then
+    echo "No server ip provided."
+    exit 1
+fi
+
+echo "Server IP: $serverip"
+
 echo "Touching frpc.toml"
 touch /etc/frp/frpc.toml
 
 echo "Editing frpc.toml"
 sed -i "s/FRP_TOKEN/$token/g" /etc/frp/frpc.toml
+sed -i "s/FRP_SERVER_IP/$serverip/g" /etc/frp/frpc.toml
 
 echo "Starting frpc"
 /usr/bin/frpc -c /etc/frp/frpc.toml
