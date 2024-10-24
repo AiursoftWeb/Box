38fba50854dd867b6a259ae71922e0415ab60cd1
Anduin Xue <anduin@aiursoft.com>
2024-10-24 12:09:21 +0000

    Update backup script to include backup of swarm-vol data and config

--- backup.sh
+++ backup.sh
@@ -6,8 +6,11 @@
 if [ -e /dev/sdc ]; then
     sudo mkdir -p /mnt/backup
     sudo mount /dev/sdc /mnt/backup
+    sudo mkdir -p /mnt/backup/backup-data/swarm-vol
+    sudo mkdir -p /mnt/backup/backup-config/swarm-vol
     
-    sudo rsync -Aavx --delete --update /swarm-vol/ /mnt/backup/swarm-vol/
+    sudo rsync -Aavx --delete --update /swarm-vol/ /mnt/backup/backup-data/swarm-vol/
+    sudo rsync -Aavx --delete --update . /mnt/backup/backup-config/swarm-vol/
     sudo umount /mnt/backup
 else
     echo "No /dev/sdc found, skipping backup..."
