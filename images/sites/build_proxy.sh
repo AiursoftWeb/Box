f66eebf46997c52e018905626990f82cfafd2ab0
Anduin Xue <anduin@aiursoft.com>
2025-07-11 04:06:31 +0000

    Refactor build_proxy.sh to improve Caddyfile construction by adding newlines for better readability

--- images/sites/build_proxy.sh
+++ images/sites/build_proxy.sh
@@ -8,7 +8,7 @@ echo "Building sites under $(pwd)..."
 find . -type f -name "*.conf" | while read -r file; do cat "$file"; echo -e "\n\n"; done | tee ./Dist/Sites.temp > /dev/null
 
 echo "Appending baseline..."
-cat ./baseline ./Dist/Sites.temp | tee ./Dist/Caddyfile > /dev/null
+(cat ./baseline; echo -e "\n\n"; cat ./Dist/Sites.temp) | tee ./Dist/Caddyfile > /dev/null
 
 echo "Caddyfile built."
 ls ./Dist/ -ashl
