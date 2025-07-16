f457d466dbabdae2d3ee6c733ed7f54d69e572e4
Anduin Xue <anduin@aiursoft.com>
2025-07-16 11:06:29 +0000

    Fix missing newline at end of file in anduins.conf

--- images/sites/build_proxy.sh
+++ images/sites/build_proxy.sh
@@ -4,6 +4,15 @@ set -e
 echo "Building Caddyfile at $(pwd)..."
 mkdir -p ./Dist
 
+echo "Adding empty lines to the end of files without a newline..."
+find . -type f -name '*.conf' | while read -r file; do
+    last_line=$(tail -n 1 "$file")
+    if [[ -n "$last_line" ]]; then
+        echo "" >> "$file"
+        echo "修复文件结尾：$file"
+    fi
+done
+
 echo "Building sites under $(pwd)..."
 find . -type f -name "*.conf" | while read -r file; do cat "$file"; echo -e "\n\n"; done | tee ./Dist/Sites.temp > /dev/null
 
