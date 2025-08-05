412a1ac9d0e29abe95ecea63db64009b9d6acf84
Anduin Xue <anduin@aiursoft.com>
2025-08-05 04:31:45 +0000

    Fix zot config link.

--- stage3/images/zot/entrypoint.sh
+++ stage3/images/zot/entrypoint.sh
@@ -5,14 +5,15 @@ set -e
 mkdir -p /config /etc/zot /data
 
 # 处理 config.json：有就用它，没就复制默认的并软链
-if [ -s /config/config.json ]; then
-  ln -sf /config/config.json /etc/zot/config.json
-else
-  if [ ! -e /config/config.json ]; then
-    cp /etc/zot/default-config.json /config/config.json
-  fi
-  ln -sf /config/config.json /etc/zot/config.json
+SRC_SETTINGS="/etc/zot/config.json"
+VOL_SETTINGS="/config/config.json"
+if [ ! -f "$VOL_SETTINGS" ]; then
+    cp $SRC_SETTINGS $VOL_SETTINGS
 fi
+if [ -f "$SRC_SETTINGS" ]; then
+    rm $SRC_SETTINGS
+fi
+ln -s $VOL_SETTINGS $SRC_SETTINGS
 
 # 启动 zot（以 root 直接跑）
 exec /usr/local/bin/zot serve /etc/zot/config.json
