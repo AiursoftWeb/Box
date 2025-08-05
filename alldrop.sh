90f19d70b3492e6c4f98f41e55c021b4390602c0
Anduin Xue <anduin@aiursoft.com>
2025-08-05 06:40:13 +0000

    Clean up all drop.sh.

--- alldrop.sh
+++ alldrop.sh
@@ -1,17 +1,5 @@
 sudo docker stack rm $(sudo docker stack ls --format '{{.Name}}')
 sleep 1
-sudo docker stop $(sudo docker ps -aq)
-sleep 1
-sudo docker kill $(sudo docker ps -aq)
-sleep 1
-sudo docker rm $(sudo docker ps -aq)
-sleep 1
-#sudo docker volume rm $(sudo docker volume ls -q) -f
-#sleep 1
-sudo docker image rm $(sudo docker image ls -aq) -f
-sleep 1
-#sudo docker network rm $(sudo docker network ls --format '{{.Name}}' | grep -v 'bridge' | grep -v 'host' | grep -v 'none')
-sleep 1
 sudo docker volume prune -f
 sleep 1
 sudo docker network prune -f
